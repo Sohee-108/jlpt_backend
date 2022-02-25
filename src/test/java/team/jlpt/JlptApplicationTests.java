a1edf404d27dd2454a4eb9ab02e1a3f3707a1581
ddohing <dnflwlq004@naver.com>
2022-02-26 00:20:25 +0900

    jlpt_backend

---
+++ src/test/java/team/jlpt/JlptApplicationTests.java
@@ -0,0 +1,13 @@
+package team.jlpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
